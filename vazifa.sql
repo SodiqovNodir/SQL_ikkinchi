c4a1df1a07311f1ec60121642d1c4447ac39438a
Nodirbek <sodiqovnodirbek14@gmail.com>
2024-11-08 19:28:07 +0500

    tayor

--- vazifa.sql
+++ vazifa.sql
@@ -1,8 +1,13 @@
 -- birinchisi
 
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS xodimlar;
+DROP TABLE IF EXISTS loyhalar;
+DROP TABLE IF EXISTS vazifalar;
+
 CREATE TABLE IF NOT EXISTS departments(
 	department_id SERIAL PRIMARY KEY,
-	nomi TEXT,
+	d_nomi TEXT,
 	joylashuv TEXT
 );
 
@@ -15,18 +20,18 @@ CREATE TABLE IF NOT EXISTS xodimlar(
 
 CREATE TABLE IF NOT EXISTS loyhalar(
 	loyha_id SERIAL PRIMARY KEY,
-	nomi TEXT,
+	l_nomi TEXT,
 	department_id INTEGER REFERENCES departments(department_id)
 );
 
 CREATE TABLE IF NOT EXISTS vazifalar(
 	vazifa_id SERIAL PRIMARY KEY,
 	tavsif TEXT,
-	xodim_id INTEGER REFERENCES xodimlar(xodim_id)
+	xodim_id INTEGER REFERENCES xodimlar(xodim_id),
 	loyha_id INTEGER REFERENCES loyhalar(loyha_id)
 );
 
-INSERT INTO departments(nomi, joylashuv) VALUES
+INSERT INTO departments(d_nomi, joylashuv) VALUES
 ('Python', 'Py xonasi'),
 ('JavaScript', 'JS xonasi'),
 ('C++', 'C+ xonasi'),
@@ -40,19 +45,23 @@ INSERT INTO xodimlar(ism, lavozim, department_id) VALUES
 ('Asad', 'Ishchi', 4),
 ('Sardor', 'Boshliq O''rin B', 3);
 
-INSERT INTO loyhalar(nomi, department_id) VALUES
+INSERT INTO loyhalar(l_nomi, department_id) VALUES
 ('Yangi jadval', 2),
-('yangi jadval2', 4)
-('yangi jadval4', 1)
-('yangi jadval3', 5)
-('yangi jadval5', 3)
+('yangi jadval2', 4),
+('yangi jadval4', 1),
+('yangi jadval3', 5),
+('yangi jadval5', 3);
 
 INSERT INTO vazifalar(tavsif, xodim_id, loyha_id) VALUES
 ('oddiy', 2, 1),
-('murakkab', 3, 2)
-('oddiy', 4, 3)
-('murakkab', 4, 5)
-('oddiy', 3, 5)
+('murakkab', 3, 2),
+('oddiy', 4, 3),
+('murakkab', 4, 5),
+('oddiy', 3, 5);
+
+SELECT ism, lavozim, d_nomi FROM xodimlar JOIN departments ON xodimlar.department_id = departments.department_id;
+SELECT l_nomi, d_nomi, joylashuv FROM loyhalar JOIN departments ON loyhalar.department_id = departments.department_id;
+SELECT tavsif, ism, l_nomi FROM vazifalar JOIN xodimlar, loyhalar ON vazifalar.xodim_id = xodimlar.xodim_id, vazifalar.loyha_id = loyhalar.loyha_id;
 
 -- -----------------------------------------------------
 -- ikkinchisi
@@ -76,6 +85,5 @@ UPDATE loyhalar SET nomi = 'Murakkab jadval' WHERE loyha_id = 2;
 DELETE FROM loyhalar WHERE loyha_id = 3;
 
 DELETE FROM xodimlar WHERE xodim_id = 4;
-
 
 
